f7fb5d8467c1934f4d89eac58e0f93dc891c5b41
emateos <emateos@PORTATIL501-PC.birchman.local>
2014-04-22 11:05:50 +0200

    Modulo Record

---
+++ Modulo/src/main/java/com/isb/modulo/components/newPackage/NuevoFlow.java
@@ -0,0 +1,15 @@
+package com.isb.modulo.components.newPackage;
+
+import java.io.Serializable;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("com_isb_modulo_components_newPackage_NuevoFlow")
+@Scope("prototype")
+public class NuevoFlow implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	// Add your methods here
+}
