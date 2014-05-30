08bb37738f39fa2c5f30c6fc0003618f36bfa376
idafereyes <idafe.reyes@servexternos.isban.es>
2014-05-30 09:35:24 +0200

    Se añade prueba de properties en input-menu.

---
+++ Tests/src/main/java/com/isb/tests/components/p/NoInputRecord.java
@@ -0,0 +1,41 @@
+package com.isb.tests.components.p;
+
+import java.io.Serializable;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.vectorsf.jvoiceframework.core.AudioItem;
+import com.vectorsf.jvoiceframework.core.Record;
+import com.vectorsf.jvoiceframework.core.Wording;
+import com.vectorsf.jvoiceframework.core.bean.factory.JvoiceBeanService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Component("com_isb_tests_components_p_NoInputRecord")
+@Scope("prototype")
+public class NoInputRecord implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Autowired
+	private JvoiceBeanService jvoiceBeanService;
+	public Record testNIRecordProg() {
+		
+		Record rec = jvoiceBeanService.getRecord();
+		
+		rec.setMaxNoInput(2);
+		
+		AudioItem ai = jvoiceBeanService.getAudioItem();		
+		Wording wd = jvoiceBeanService.getWording();		
+		wd.setText("Probando no input en record programatico");
+		ai.setWording(wd);
+		
+		rec.getAudioItems().add(ai);
+
+		
+		
+		return rec;
+	}
+
+	// Add your methods here
+}
---
+++ Tests/src/main/jvoice-gen/p/InputPropertiesMessages.java
@@ -0,0 +1,7 @@
+package \p\;
+
+import ch.qos.cal10n.BaseName;
+
+@BaseName("\p\.InputPropertiesMessages")
+public enum InputPropertiesMessages {
+}
