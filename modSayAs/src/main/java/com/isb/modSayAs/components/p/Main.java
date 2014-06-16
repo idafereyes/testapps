7ed91ff0d52bddf9d87d9eabf74f29443a4200c4
idafereyes <idafe.reyes@servexternos.isban.es>
2014-06-16 10:23:44 +0200

    Más formatos en numset time.

--- modSayAs/src/main/java/com/isb/modSayAs/components/p/Main.java
+++ modSayAs/src/main/java/com/isb/modSayAs/components/p/Main.java
@@ -4,12 +4,20 @@ import java.io.Serializable;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import com.vectorsf.jvoiceframework.core.Record;
+import com.vectorsf.jvoiceframework.core.bean.factory.JvoiceBeanService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
 @Scope("prototype")
 public class Main implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	@Autowired
+	private JvoiceBeanService jvoiceBeanService;
+	public Record state() {
+		return jvoiceBeanService.getRecord();
+	}
 
 	// Add your methods here
 }
